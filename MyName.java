fdc07eb2708b82dcd69f3124c1c480daa96548dd
“xzy” <824838674@qq.com>
2021-06-19 16:24:00 +0800

    myname

---
+++ MyName.java
@@ -0,0 +1,21 @@
+//MyName类放在包下
+package com.java28.myname;
+
+//类模板注释
+/**
+ * @Project: java_base
+ * @Site: http://www.lemonban.com
+ * @Forum: http://testingpai.com
+ * @Copyright: ©2021 柠檬班Java28期
+ * @Author: Administrator
+ * @Create: 2021-03-23 14:37
+ * @Desc： 打印我的名字
+ **/
+//定义一个公共MyName类（类是Java中最小代码编写的单元）
+public class MyName {
+    //main方法程序的入口（该格式固定）
+    public static void main(String[] args){
+    //打印输出下面的语句，括号里面用"",语句结束后面记得用;结尾
+    System.out.println("谢智勇");
+    }
+}
